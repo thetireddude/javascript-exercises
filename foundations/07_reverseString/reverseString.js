295fa0d809ab2f2c1b284fb99042f357b6be4705
tired_ <omaralim06@gmail.com>
2026-05-27 19:08:57 -0700

    feat: complete javascript foundations exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result = ""
+    for(i=string.length-1; i >= 0; i--) {
+        result += string[i]
+    }
+    return result
 };
 
 // Do not edit below this line
