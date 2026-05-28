0ef68e7878afc9fb5e4f93749149e03bdaf3f4ca
tired_ <omaralim06@gmail.com>
2026-05-27 18:56:03 -0700

    feat: complete javacript foundations exercise 05

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,9 +8,15 @@
 */
 
 // Add your code right below, good luck!
+firstName = "Carlos"
+lastName = 'Stevenson'
+thisYear = 1965
+birthYear = 1947
+
+greeting = "Hello! My name is " + firstName + " " + lastName + " and I am  " + (thisYear - birthYear) + " years old."
 
 
-
+console.log(greeting)
 
 
 // Do not change this
